e076ab4e05cb5daf70fbe3324ad3a07196b26eac
baconstarvation <ant.ucd@gmail.com>
2014-03-03 23:53:32 -0800

    finished up scope. onto controllers now

--- exapples/scope-example/myApp.js
+++ exapples/scope-example/myApp.js
@@ -1,4 +1,13 @@
+angular.module("myApp", [])
+.controller('MyController',
+function($scope) {
+	$scope.name = "baconator";
+});
+
+
+/* using $rootScope
 angular.module('myApp', [])
 	.run(function($rootScope) {
 		$rootScope.name = "World";
 });
+*/
