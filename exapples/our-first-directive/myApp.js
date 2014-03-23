08744db4525339c3f428494c65fb473497929db5
baconstarvation <ant.ucd@gmail.com>
2014-03-23 10:44:37 -0700

    on 'Our First Directive'

---
+++ exapples/our-first-directive/myApp.js
@@ -0,0 +1,8 @@
+var app = angular.module('myApp', [])
+.directive('myDirective', function() {
+	return {
+		restrict: 'E',
+		template: '<a href="http://google.com">
+		Click me to go to Google</a>'
+	}
+});
