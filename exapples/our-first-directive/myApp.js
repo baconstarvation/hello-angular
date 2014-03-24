3b8d37bd1f555da28bf344ea474bd83b78013303
baconstarvation <ant.ucd@gmail.com>
2014-03-24 07:59:21 -0700

    Skipping 'Our First Directive' for now. moving onto Expressions (pg. 61) to make use of another live example

--- exapples/our-first-directive/myApp.js
+++ exapples/our-first-directive/myApp.js
@@ -1,3 +1,5 @@
+// Skipping 'Our First Directive' for now. moving onto Expressions (pg. 61) to make use of another live example
+
 var app = angular.module('myApp', [])
 .directive('myDirective', function() {
 	return {
